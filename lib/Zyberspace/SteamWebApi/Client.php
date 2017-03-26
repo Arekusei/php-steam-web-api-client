46915e8bbe492d090511d6c433c969d1340f772c
Arekusei <jack.dark.by@gmail.com>
2017-03-26 17:32:06 +0300

    In the client has been added the ability to specify the settings. Client refactoring.

--- lib/Zyberspace/SteamWebApi/Client.php
+++ lib/Zyberspace/SteamWebApi/Client.php
@@ -6,26 +6,62 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 namespace Zyberspace\SteamWebApi;
+
+use GuzzleHttp\ClientInterface;
 
 class Client
 {
     const BASE_URL = 'https://api.steampowered.com/';
 
+    /**
+     * @var string
+     */
     protected $_apiKey;
 
     /**
-     * @var \GuzzleHttp\Client
+     * @var \GuzzleHttp\ClientInterface
      */
-    protected $_guzzleClient;
+    protected $_httpClient;
+    /**
+     * @var array
+     */
+    protected $_clientOptions;
 
-    public function __construct($apiKey)
+    public function __construct($apiKey, $clientOptions = array())
     {
         $this->_apiKey = $apiKey;
+        $this->_clientOptions = $clientOptions;
+    }
 
-        $this->_guzzleClient = new \GuzzleHttp\Client(array(
-            'base_uri' => self::BASE_URL
-        ));
+    /**
+     * @param ClientInterface $httpClient
+     * @return $this
+     */
+    public function setHttpClient(ClientInterface $httpClient)
+    {
+        $this->_httpClient = $httpClient;
+        return $this;
+    }
+
+    /**
+     * @return \GuzzleHttp\ClientInterface
+     */
+    public function getHttpClient()
+    {
+        if ($this->_httpClient === null) {
+            $this->_httpClient = $this->createDefaultClient();
+        }
+        return $this->_httpClient;
+    }
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    protected function createDefaultClient()
+    {
+        return new \GuzzleHttp\Client(array_merge(array('base_uri' => self::BASE_URL), $this->_clientOptions));
     }
 
     public function call($interface, $method, $version, $httpMethod, $parameters)
@@ -33,7 +69,7 @@ class Client
         $path = implode('/', array($interface, $method, 'v' . $version));
 
         $parameters['key'] = $this->_apiKey;
-        $response = $this->_guzzleClient->request($httpMethod, $path, array(
+        $response = $this->getHttpClient()->request($httpMethod, $path, array(
             'query' => $parameters
         ));
 
